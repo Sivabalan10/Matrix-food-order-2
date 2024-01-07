a5b65f5d8eb93cec04915411988e2a011f9e6bee
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2024-01-07 12:55:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,129 @@ function order(link){
         console.log("Redirecting");
         window.location.href= b;
 }}
+
+const a = [1,1,0,1];
+// 0 - #Hotel_1, 1 - #Hotel_2, 2 - #Hotel_3,3 - #Hotel_4
+console.log("Status - Sunday");
+console.log("Automation - ON");
+setInterval(automate,1000);
+function automate(){
+    const d = new Date();
+    let day = d.getDay();
+    let Hours = d.getHours();
+    let min = d.getMinutes();
+    let pand_open = document.getElementById('pan_open'); 
+    let pand_clo = document.getElementById('pan_clo');
+    let taj_open = document.getElementById('taj_open'); 
+    let taj_clo = document.getElementById('taj_clo');
+    let pot_open = document.getElementById('pot_open');
+    let pot_clo = document.getElementById('pot_clo');
+    let rem_open = document.getElementById('rem_open');
+    let rem_clo = document.getElementById('rem_clo');
+    let rem1_open = document.getElementById('rem1_open');
+    let rem1_clo = document.getElementById('rem1_clo');
+    let rem2_open = document.getElementById('rem2_open');
+    let rem2_clo = document.getElementById('rem2_clo');
+
+    if(day == 0){
+        if(((Hours == 9)&&(min>=30))||(Hours >= 10)) {
+            if(a[0] == 1){
+                if((Hours >= 11)&&(min>=30)){
+                    pand_clo.style.display = "inline-block";
+                    pand_open.style.display = "none";
+                }
+                else if(Hours > 11){
+                    pand_clo.style.display = "inline-block";
+                    pand_open.style.display = "none";
+                }
+                else{
+                    pand_open.style.display = "inline-block";
+                    pand_clo.style.display = "none";
+                }
+            }
+            if(a[1] == 1){
+                if(Hours >= 12){
+                    taj_clo.style.display = "inline-block";
+                    taj_open.style.display = "none";
+                }
+                else{
+                    taj_open.style.display = "inline-block";
+                    taj_clo.style.display = "none";
+                }
+            }
+            if(a[2] == 1){
+                if((Hours >= 11)&&(min>=30)){
+                    pot_clo.style.display = "inline-block";
+                    pot_open.style.display = "none";
+                }
+                else if(Hours > 11){
+                    pot_clo.style.display = "inline-block";
+                    pot_open.style.display = "none";
+                }
+                else{
+                    pot_open.style.display = "inline-block";
+                    pot_clo.style.display = "none";
+                }
+            }
+            if(a[3] == 1){
+                if((Hours >= 11)&&(min>=30)){
+                    rem_clo.style.display = "inline-block";
+                    rem_open.style.display = "none";
+                    rem1_clo.style.display = "inline-block";
+                    rem1_open.style.display = "none";
+                    rem2_clo.style.display = "inline-block";
+                    rem2_open.style.display = "none";
+                }
+                else if(Hours > 11){
+                    rem_clo.style.display = "inline-block";
+                    rem_open.style.display = "none";
+                    rem1_clo.style.display = "inline-block";
+                    rem1_open.style.display = "none";
+                    rem2_clo.style.display = "inline-block";
+                    rem2_open.style.display = "none";
+                }
+                else{
+                    rem_open.style.display = "inline-block";
+                    rem_clo.style.display = "none";
+                    rem1_open.style.display = "inline-block";
+                    rem1_clo.style.display = "none";
+                    rem2_open.style.display = "inline-block";
+                    rem2_clo.style.display = "none";
+                }
+            }
+           
+            
+        }
+        else{
+            pand_clo.style.display = "inline-block";
+            pand_open.style.display = "none";
+            taj_clo.style.display = "inline-block";
+            taj_open.style.display = "none";
+            pot_clo.style.display = "inline-block";
+            pot_open.style.display = "none";
+            rem_clo.style.display = "inline-block";
+            rem_open.style.display = "none";
+            rem1_clo.style.display = "inline-block";
+            rem1_open.style.display = "none";
+            rem2_clo.style.display = "inline-block";
+            rem2_open.style.display = "none";
+        }
+
+    }
+    else{
+        pand_clo.style.display = "inline-block";
+        pand_open.style.display = "none";
+        taj_clo.style.display = "inline-block";
+        taj_open.style.display = "none";
+        pot_clo.style.display = "inline-block";
+        pot_open.style.display = "none";
+        rem_clo.style.display = "inline-block";
+        rem_open.style.display = "none";
+        rem1_clo.style.display = "inline-block";
+        rem1_open.style.display = "none";
+        rem2_clo.style.display = "inline-block";
+        rem2_open.style.display = "none";
+    }
+    
+
+}
